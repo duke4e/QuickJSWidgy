7c86489f361f844ef1fcbc3a8d1e2a0c649d784b
duke4e <duke4e@gmail.com>
2022-04-18 22:22:08 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "QuickJS",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+    ],
+    products: [
+        .library(name: "QuickJS", targets: ["QuickJS"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QuickJSC",
+            dependencies: [],
+            path: "Sources/QuickJSC"),
+        .target(
+            name: "QuickJS",
+            dependencies: ["QuickJSC"])
+    ]
+)
